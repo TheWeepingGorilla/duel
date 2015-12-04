e313e16d49c2fd92f029b6e6a7b92d79e9de7d1f
Sean Morrison <seanmorrison112358@gmail.com>
2015-12-04 11:41:56 -0800

    Add route for duel & link on index

--- routes/index.js
+++ routes/index.js
@@ -4,46 +4,50 @@ var passport = require('passport');
 var Account = require('../models/account');
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/')
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/')
 }
 
 router.get('/', function (req, res) {
-    res.render('index', { user : req.user });
+		res.render('index', { user : req.user });
 });
 
 router.get('/register', function(req, res) {
-    res.render('register', { });
+		res.render('register', { });
 });
 
 router.post('/register', function(req, res) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-        if (err) {
-          return res.render("register", {info: "Sorry. That username already exists. Try again."});
-        }
-        passport.authenticate('local')(req, res, function () {
-            res.redirect('/');
-        });
-    });
+		Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+				if (err) {
+					return res.render("register", {info: "Sorry. That username already exists. Try again."});
+				}
+				passport.authenticate('local')(req, res, function () {
+						res.redirect('/');
+				});
+		});
 });
 
 router.get('/login', function(req, res) {
-    res.render('login', { user : req.user });
+		res.render('login', { user : req.user });
 });
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/');
+		res.redirect('/');
 });
 
 router.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
+	req.logout();
+	res.redirect('/');
 });
 
-router.get('/ping', ensureAuthenticated, function(req, res){
-    res.status(200).send("pong!");
+router.get('/duel', ensureAuthenticated, function(req, res) {
+	res.render('duel', { user : req.user });
+});
+
+router.get('/ping', ensureAuthenticated, function(req, res) {
+		res.status(200).send("pong!");
 });
 
 module.exports = router;
